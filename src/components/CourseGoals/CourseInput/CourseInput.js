1a7f4f3987eb9cf2e749860005051ba85748bb7d
SK <sarthakkarandikar03@gmail.com>
2022-12-15 08:42:52 +0000

    Dec 15, 2022, 2:12 PM

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -8,7 +8,7 @@ const CourseInput = props => {
   const [isValid, setIsValid] = useState(true)
 
   const goalInputChangeHandler = event => {
-    if (event.target.value.trim.length === 0) {
+    if (event.target.value.trim().length === 0) {
       setIsValid(true)
     }
     setEnteredValue(event.target.value);
@@ -16,7 +16,7 @@ const CourseInput = props => {
 
   const formSubmitHandler = event => {
     event.preventDefault();
-    if (enteredValue.trim.length === 0) {
+    if (enteredValue.trim().length === 0) {
       setIsValid(false)
       return
     }
